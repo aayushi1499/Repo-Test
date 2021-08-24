193e9186a4d48e74658e0424afe40e1f4ab044a1
Aayushi Bansal <aayushi1499@gmail.com>
2021-08-24 22:14:45 +0530

    Update finalnotice.php

--- finalnotice.php
+++ finalnotice.php
@@ -18,7 +18,7 @@ if (!isset($_SESSION['USERNAMES'])){
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
      <link rel="stylesheet" href="../assests/css/style.css">
 
-     <title>Grievance Redressal</title>
+     <title>Grievance Redressal System</title>
    </head>
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
      <span class="navbar-brand mb-0 h1">Grievance Redressal</span>
@@ -59,7 +59,7 @@ if (!isset($_SESSION['USERNAMES'])){
            $statement = $conn->prepare($sql);
            $statement->execute();
            $result = $statement->fetchAll();
-
+                
            foreach ($result as $key => $value) {}
               $y = $value['level'];
               if ($y =='University') {?>
@@ -98,7 +98,6 @@ if (!isset($_SESSION['USERNAMES'])){
         }
 </script>
      <!-- Optional JavaScript -->
-     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
