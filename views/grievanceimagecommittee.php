2aea01a5cbb14ddacf01f461e9eef008a2f894e1
Aayushi Bansal <aayushi1499@gmail.com>
2021-08-31 20:24:52 +0530

    Update grievanceimagecommittee.php

--- views/grievanceimagecommittee.php
+++ views/grievanceimagecommittee.php
@@ -30,7 +30,7 @@ if (!isset($_SESSION['USERNAMEC'])){
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
      <link rel="stylesheet" href="../assests/css/style.css">
 
-     <title>Grievance Redressal</title>
+     <title>Grievance Redressal System</title>
    </head>
    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
      <span class="navbar-brand mb-0 h1">Grievance Redressal</span>
@@ -53,15 +53,14 @@ if (!isset($_SESSION['USERNAMEC'])){
 
      <div class="container">
        <?php if (empty($value['image'])): ?>
-         <h5 class="lead text-center display-4">NO Attachement</h5>
+         <h5 class="lead text-center display-4">NO Image Attachement</h5>
 
        <?php else: ?>
-         <img src="../uploads/<?php echo $value['image']; ?>" alt="Grievance File" class="img-fluid rounded mx-auto d-block border border-dark" width="1250" height="800">
+         <img src="./uploads/<?php echo $value['image']; ?>" alt="Grievance File" class="img-fluid rounded mx-auto d-block border border-dark" width="1250" height="800">
 
        <?php endif; ?>
      </div>
      <!-- Optional JavaScript -->
-     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
      <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
