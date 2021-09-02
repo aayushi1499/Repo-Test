64564fc88a6a4d6a242251d909cb42c35fcdb6d8
Aayushi Bansal <aayushi1499@gmail.com>
2021-09-02 20:21:09 +0530

    Update committeemembersdashboard.php

--- committeemembersdashboard.php
+++ committeemembersdashboard.php
@@ -115,8 +115,8 @@ if (!isset($_SESSION['USERNAMEC'])){
 <script type="text/javascript">
     /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
         function openNav() {
-          document.getElementById("mySidenav").style.width = "250px";
-          document.getElementById("main").style.marginLeft = "250px";
+          document.getElementById("mySidenav").style.width = "290px";
+          document.getElementById("main").style.marginLeft = "350px";
         }
 
         /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
